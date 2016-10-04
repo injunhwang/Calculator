6a8af892d818cdd0062422074dd833eb2de89cc5
ssk316@ajou.ac.kr <ssk316@ajou.ac.kr>
2016-10-04 11:06:54 +0900

    commit

--- LAB4_201220045/src/Calculator.java
+++ LAB4_201220045/src/Calculator.java
@@ -1,22 +1,30 @@
 
 public class Calculator {
 
-	public double add (double x, double y){
-		return x+y;
+	public double add(double x, double y) {
+		return x + y;
+	}
+
+	public double subtract(double x, double y) {
+
+		return x - y;
+	}
+
+	public double multiply(double x, double y) {
+		return x * y;
+	}
+
+	public double divide(double x, double y) {
+		if (y != 0) {
+			return x / y;
+		} else {
+			System.out.print("ERROR");
+			return 0;
 		}
-	
-	public double subtract (double x, double y){
-		return 0;
-		}
-	
-	public double multiply (double x, double y){
-		return x*y;
-		}
-	
-	public double divide (double x, double y){
-		return 0;
-		}
-	public String string(){
+
+	}
+
+	public String string() {
 		return "ChanghyukIm_201120218, InjunHwang_201220045, SungkwonSon_201323152\nhttps://github.com/injunhwang/Calculator ";
 	}
 }
