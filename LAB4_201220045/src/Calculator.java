3b48a5b6f47652306afe78d7742e680d2fb4387a
USER <USER@LG>
2016-10-04 11:29:10 +0900

    aaa

--- LAB4_201220045/src/Calculator.java
+++ LAB4_201220045/src/Calculator.java
@@ -6,13 +6,11 @@ public class Calculator {
 	}
 
 	public double subtract(double x, double y) {
-
 		return x - y;
 	}
 
 	public double multiply(double x, double y) {
 		return x * y;
-		
 	}
 
 	public double divide(double x, double y) {
