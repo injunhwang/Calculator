eae23aad6567d3f05c4629cd05262eb9ab2c24f7
USER <USER@LG>
2016-10-04 11:25:52 +0900

    we

--- LAB4_201220045/src/Calculator.java
+++ LAB4_201220045/src/Calculator.java
@@ -12,6 +12,7 @@ public class Calculator {
 
 	public double multiply(double x, double y) {
 		return x * y;
+		
 	}
 
 	public double divide(double x, double y) {
--- LAB4_201220045/src/CalculatorTest.java
+++ LAB4_201220045/src/CalculatorTest.java
@@ -22,7 +22,9 @@ public class CalculatorTest {
 
 		String s = cal.string();
 		System.out.println(s);
+		
 
+		
 	}
 
 }
