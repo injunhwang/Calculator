6dd2975e180dec52e5d4c5e0c66d9eccd548e0e9
USER <USER@LG>
2016-10-04 10:35:23 +0900

    hello

--- LAB4_201220045/src/Calculator.java
+++ LAB4_201220045/src/Calculator.java
@@ -4,4 +4,16 @@ public class Calculator {
 	public double add (double x, double y){
 		return 0;
 		}
+	
+	public double suntract (double x, double y){
+		return 0;
+		}
+	
+	public double multiply (double x, double y){
+		return 0;
+		}
+	
+	public double divide (double x, double y){
+		return 0;
+		}
 }
