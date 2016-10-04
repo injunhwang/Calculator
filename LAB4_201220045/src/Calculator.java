fb7204ccde23148c6be9290d604093be81c7617e
im <im@172.30.50.29>
2016-10-04 10:56:02 +0900

    Programmer A

--- LAB4_201220045/src/Calculator.java
+++ LAB4_201220045/src/Calculator.java
@@ -2,18 +2,21 @@
 public class Calculator {
 
 	public double add (double x, double y){
-		return 0;
+		return x+y;
 		}
 	
-	public double suntract (double x, double y){
+	public double subtract (double x, double y){
 		return 0;
 		}
 	
 	public double multiply (double x, double y){
-		return 0;
+		return x*y;
 		}
 	
 	public double divide (double x, double y){
 		return 0;
 		}
+	public String string(){
+		return "ChanghyukIm_201120218, InjunHwang_201220045, SungkwonSon_201323152\nhttps://github.com/injunhwang/Calculator ";
+	}
 }
